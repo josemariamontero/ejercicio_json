a5e13474c1e63f7c3df8dad52dc9c699874890b6
Jose Maria Montero <josema9817@gmail.com>
2019-03-04 19:43:48 +0100

    Queda corregir descripción

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,16 +2,24 @@ import json
 with open('confiterias.json','r') as fichero:
 	doc = json.load(fichero)
 
+indicador = False
 direccion = input("Dime la calle de una confiteria: ")
 
 for i in doc["directorios"]["directorio"]:
-	ubicacion_past = i["direccion"]
-	if direccion in ubicacion_past:
-		descripcion = i["descripcion"]["content"]
+	if direccion in i["direccion"]:
+		nombre = i["nombre"]["content"]
+		indicador = True
+		descripcion = i["descripcion"]
 		telefono = i["telefono"]["content"]
 		foto = i["foto"]["content"]
-		print (foto)
+	if not indicador:
+		print("No existe esa dirección")
 
+print(nombre)
+print("---------------------")
+print("Descripción:",descripcion)
+print("Telefono:",telefono)
+print("Foto:",foto)
 	#print(ubicacion_past[0])
 
 #Libertad, 2 - 33206 Gijón
