3d5f7f579697596e1b172b4d3e330ff780bfe4c6
Jose Maria Montero <josema9817@gmail.com>
2019-03-04 19:22:57 +0100

    Descripción,tlf y fotode nuevo

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,8 +2,16 @@ import json
 with open('confiterias.json','r') as fichero:
 	doc = json.load(fichero)
 
-#direccion = input("Dime la calle de una confiteria: ")
+direccion = input("Dime la calle de una confiteria: ")
 
 for i in doc["directorios"]["directorio"]:
 	ubicacion_past = i["direccion"]
-	print(ubicacion_past[0])
+	if direccion in ubicacion_past:
+		descripcion = i["descripcion"]["content"]
+		telefono = i["telefono"]["content"]
+		foto = i["foto"]["content"]
+		print (foto)
+
+	#print(ubicacion_past[0])
+
+#Libertad, 2 - 33206 Gijón
